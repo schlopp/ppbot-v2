8d1488ef9694d052e16ed50657aee83199c4b8ac
schlöpp! <67605203+schlopp@users.noreply.github.com>
2021-03-17 13:46:02 +0100

    Why was random here lmao

--- userdata.py
+++ userdata.py
@@ -1,60 +1,11 @@
 import asyncpg
 import discord
-import random
 import toml
 from discord.ext import commands
 
 with open("config.toml") as f:
     config = toml.loads(f.read())
 
-class SQLMethodError(Exception):
-    """SQL Method Error"""
-    
-    def __init__(self, method):
-        self.method = method
-        super().__init__('SQL Method unkown')
-    
-    def __str__(self):
-        return f'\033[91mSQL Method: "{self.method}" unknown\033[0m'
-
-async def printfetch(pgselect:str,pgfrom:str,pgwhere:str=None):
-    """prints the return value of a fetched premade-sql statement\n\n__\n\nsql statement:\n\n- `SELECT {pgselect} FROM {pgfrom}( WHERE {pgwhere}; || ;  )`"""
-    pgwhere = ";" if pgwhere == None else f" WHERE {pgwhere};"
-    conn = await asyncpg.connect(config['admin']['PSQL'])
-    fetched = await conn.fetch(
-        f'''
-        SELECT {pgselect} FROM {pgfrom}{pgwhere}
-        '''
-        )
-    for i in fetched:
-        print(dict(i))
-    return await conn.close()
-
-async def fetch(pgselect:str,pgfrom:str,pgwhere:str=None):
-    """returns the return value of a fetched premade-sql statement\n\n__\n\nsql statement:\n\n- `SELECT {pgselect} FROM {pgfrom}( WHERE {pgwhere}; || ;  )`"""
-    pgwhere = " WHERE {pgwhere};" if pgwhere else f";"
-    conn = await asyncpg.connect('postgres://postgres:MPIWL32531@localhost:3626/ppbot')
-    fetched = await conn.fetch(
-        f'''
-        SELECT {pgselect} FROM {pgfrom}{pgwhere}
-        '''
-        )
-    await conn.close()
-    return [dict(i) for i in fetched]
-
-async def runsql(method:str,sqlstring:str):
-    conn = await asyncpg.connect(config['admin']['PSQL'])
-    if method == "execute":
-        await conn.execute(sqlstring)
-        return await conn.close()
-    elif method == "fetch":
-        fetched = await conn.fetch(sqlstring)
-        await conn.close()
-        return fetched
-    else:
-        await conn.close()
-        raise SQLMethodError(method=method)
-    
 class Pp:
     def __init__(self, user_id:int):
         self.user_id = user_id
