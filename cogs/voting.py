0c85303bdcb14f9b6c075343a2d4e7148c32198c
schlöpp! <67605203+schlopp@users.noreply.github.com>
2021-04-06 04:06:16 +0200

    Agressive Advertising™️

--- cogs/voting.py
+++ cogs/voting.py
@@ -20,9 +20,10 @@ class TopGG(commands.Cog):
     @commands.command(aliases=["vote","voted","upvote",])
     async def vote_check(self,ctx):
         voted = await get_user_topgg_vote(self.bot, ctx.author.id)
+        advert = "**free 2x boost and other perks!**"
         if voted:
-            return await ctx.send('You\'ve voted for pp bot on https://top.gg/bot/735147633076863027')
-        return await ctx.send('You haven\'t voted for pp bot on https://top.gg/bot/735147633076863027')
+            return await ctx.send(f'You\'ve voted for pp bot on https://top.gg/bot/735147633076863027\nHave fun with your {advert}')
+        return await ctx.send(f'You haven\'t voted for pp bot on https://top.gg/bot/735147633076863027\nYou\'re missing out on a {advert} It\'s free and takes 10 seconds!')
     
 def setup(bot):
     bot.add_cog(TopGG(bot))
