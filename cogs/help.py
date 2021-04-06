4b194ae50eb74bc0b2672ea5dd12f7f57585f1a7
schlöpp! <67605203+schlopp@users.noreply.github.com>
2021-04-06 14:16:40 +0200

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -25,7 +25,7 @@ class helping(commands.Cog):
         embed                   = discord.Embed(colour=discord.Colour(random.choice([0x008000, 0xffa500, 0xffff00])))
         
         embed.title             = "help | " + advert
-        embed.url               = "https://discord.com/oauth2/authorize?client_id=735147633076863027&scope=bot&permissions=70634560"
+        embed.url               = "https://discord.com/oauth2/authorize?client_id=735147633076863027&scope=bot&permissions=2147863616"
         embed.description       = "pp bot. Grow your own pp today!"
         embed.set_thumbnail(url = "https://cdn.discordapp.com/avatars/735147633076863027/a34d69e6d5e2021fc64caa69f642e37b.webp?size=1024")
         embed.set_footer(text   = "\""+random.choice(["big pp gang","my gf has a bigger pp than me","lmao wtf is this bot","i love my wife"])+"\"")
@@ -40,42 +40,42 @@ class helping(commands.Cog):
         embed.add_field(
             name   = "**Shop commands**",
             value  = """
-`shop, buy <amount> <item>`
+[`shop, buy <amount> <item>`](https://discord.com/oauth2/authorize?client_id=735147633076863027&scope=bot&permissions=2147863616)
             """,
         inline = True
         )
         embed.add_field(
             name   = "**Gambling commands**",
             value  = """
-`gamble <amount>`
+[`gamble <amount>`](https://discord.com/oauth2/authorize?client_id=735147633076863027&scope=bot&permissions=2147863616)
             """,
         inline = True
         )
         embed.add_field(
             name   = "**Fun commands**",
             value  = """
-`cat, dog, fox, redpanda, panda`
+[`cat, dog, fox, redpanda, panda`](https://discord.com/oauth2/authorize?client_id=735147633076863027&scope=bot&permissions=2147863616)
             """,
         inline = True
         )
         embed.add_field(
             name   = "**Extra economy commands**",
             value  = """
-`hospital, fish, hunt, beg, daily`
+[`hospital, fish, hunt, beg, daily`]
             """,
         inline = True
         )
         embed.add_field(
             name   = "**Utility commands**",
             value  = """
-`leaderboard <user>, clear <amount> <user>|<r <regex>>`
+[`leaderboard <user>, suggest, github, vote`](https://discord.com/oauth2/authorize?client_id=735147633076863027&scope=bot&permissions=2147863616)
             """,
         inline = True
         )
         embed.add_field(
             name   = "**Help commands**",
             value  = """
-`help, invite, support, suggest, github`
+[`help, invite, support`](https://discord.com/oauth2/authorize?client_id=735147633076863027&scope=bot&permissions=2147863616)
             """,
         inline = True
         )
