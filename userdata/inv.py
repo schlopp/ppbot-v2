e22c7cf0cbfe4e3cc4684919507b0cbeccdf1917
schlopp <god.tcs@gmail.com>
2021-05-09 14:43:01 +0200

    mmmm

--- userdata/inv.py
+++ userdata/inv.py
@@ -5,43 +5,112 @@ with open("./config.toml") as f:
     config = toml.loads(f.read())
 
 
-class Inv:
+class Inv(dict):
     def __init__(self, user_id:int):
         self.user_id = user_id
+        self.new_items = []
     
+    def __getitem__(self, key):
+        if key in self:
+            return self.get(key)
+        self.new_items.append(key)
+        self[key] = 0
+        return 0
     
-    async def fetch_all(self):
+    async def __aenter__(self):
         conn = await asyncpg.connect(config['admin']['PSQL'])
         fetched = await conn.fetch('''SELECT * FROM userdata.inv WHERE user_id = $1''',self.user_id)
         await conn.close()
         items = {}
         for i in fetched:
-            items[dict(i)["item_name"]] = dict(i)["amount"]
-        return items if items else {}
+            self[dict(i)["item_name"]] = dict(i)["amount"]
+        self.old_inv = self
+        return self
     
+    async def __aexit__(self, exc_type, exc, tb):
+        conn = await asyncpg.connect(config['admin']['PSQL'])
+        insertvalues = ''
+        updatevalues = []
+        
+        for key, value in self.items():
+            if key in self.new_items:
+                insertvalues += f'({self.user_id}, {key}, {value}),'
+            elif self[key] != self.old_inv[key]:
+                updatevalues.append((key,value))
+                
+        if insertvalues:
+            await conn.execute('''
+                INSERT INTO userdata.inv (user_id, item_name, amount)
+                VALUES
+                    $2
+                ON CONFLICT
+                    (user_id, item_name)
+                    DO NOTHING;
+            ''',
+            self.user_id,insertvalues.rstrip(','))
+            
+        for i in updatevalues:
+            await conn.execute('''
+                UPDATE userdata.inv
+                SET
+                    amount = $1
+                WHERE
+                    item_name = $2 AND user_id = $3;
+                ''',
+                i[0],
+                i[1],
+                self.user_id)
+        await conn.close()
+    
+    @classmethod
+    async def fetch(cls, user_id):
+        self = cls(user_id)
+        conn = await asyncpg.connect(config['admin']['PSQL'])
+        fetched = await conn.fetch('''SELECT * FROM userdata.inv WHERE user_id = $1''',self.user_id)
+        await conn.close()
+        items = {}
+        for i in fetched:
+            self[dict(i)["item_name"]] = dict(i)["amount"]
+        self.old_inv = self
+        return self
+    
+    async def update(self):
+        conn = await asyncpg.connect(config['admin']['PSQL'])
+        insertvalues = ''
+        updatevalues = []
+        
+        for key, value in self.items():
+            if key in self.new_items:
+                insertvalues += f'({self.user_id}, {key}, {value}),'
+            else:
+                updatevalues.append((key,value))
+                
+        if insertvalues:
+            await conn.execute('''
+                INSERT INTO userdata.inv (user_id, item_name, amount)
+                VALUES
+                    $2
+                ON CONFLICT
+                    (user_id, item_name)
+                    DO NOTHING;
+            ''',
+            self.user_id,insertvalues.rstrip(','))
+            
+        for i in updatevalues:
+            await conn.execute('''
+                UPDATE userdata.inv
+                SET
+                    amount = $1
+                WHERE
+                    item_name = $2 AND user_id = $3;
+                ''',
+                i[0],
+                i[1],
+                self.user_id)
+        await conn.close()
     
     async def has_item(self, item_name:str) -> bool:
         conn = await asyncpg.connect(config['admin']['PSQL'])
         fetched = await conn.fetch('''SELECT * FROM userdata.inv WHERE user_id = $1 AND item_name = $2''',self.user_id,item_name)
         await conn.close()
         return True if fetched and dict(fetched[0])["amount"]>0 else False
-    
-    
-    async def new_item(self, item_name:str, amount:int=1):
-        conn = await asyncpg.connect(config['admin']['PSQL'])
-        await conn.execute('''
-            INSERT INTO userdata.inv (user_id, item_name, amount) VALUES ($1, $2, $3)
-            ON CONFLICT (user_id, item_name) DO
-            UPDATE SET amount = userdata.inv.amount + $3;
-        ''',
-        self.user_id,           #$1 user_id
-        item_name,              #$2 item_name
-        amount)                 #$3 amount
-        return await conn.close()
-    
-    
-    async def fetch_item(self, item_name:str):
-        conn = await asyncpg.connect(config['admin']['PSQL'])
-        fetched = await conn.fetch('''SELECT * FROM userdata.inv WHERE user_id = $1 AND item_name = $2''',self.user_id,item_name)
-        await conn.close()
-        return dict(fetched[0]) if fetched else {"user_id":self.user_id,"item_name":item_name,"amount":0}
