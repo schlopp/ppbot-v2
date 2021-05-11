5f2dafbae347caeaeaddeb362873c2b2c17346da
schlöpp! <67605203+schlopp@users.noreply.github.com>
2021-05-11 16:03:12 +0200

    Update fan_club_commands.py

--- cogs/fan_club_commands.py
+++ cogs/fan_club_commands.py
@@ -1,1 +1,35 @@
+import discord
+from discord.ext import commands
+import random
 
+
+class FanClubCommands(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+    
+    async def cog_check(self, ctx):
+      return ctx.guild.id == 735241297320738826
+    
+    
+    @commands.command()
+    async def destroy(self, ctx, user:discord.Member):
+      random_object = random.choice(
+        [
+          "Two Trucks",
+          "Schlopp's Face",
+          "The Impostor",
+          "Knowledge",
+          "A 19 Dollar Fortnite Card",
+          "Big Hot Men",
+          "Coffee",
+          "Ghigeon",
+          "Knives",
+          "Some Orphans",
+        ]
+      )
+      return await ctx.send(f'destroys **{user.display_name}** with {}')
+      
+
+def setup(bot):
+    bot.add_cog(FanClubCommands(bot))
