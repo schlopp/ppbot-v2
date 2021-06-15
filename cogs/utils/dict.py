a90806835bfec2aec02ca9e71ff4bcbbe09d0c52
schlopp <god.tcs@gmail.com>
2021-06-15 17:28:01 +0200

    created dict.py as a modified dictionary class

---
+++ cogs/utils/dict.py
@@ -0,0 +1,29 @@
+import typing
+from json import loads as jsonloads
+
+class Dict(dict):
+    """
+    Modified dict
+    """
+
+    def __init__(self, *args, **kwargs ):
+        dict.__init__(self, *args, **kwargs )
+    
+    @classmethod
+    def from_json(cls, json:str):
+        return cls(jsonloads(json))
+    
+    def __repr__(self):
+        return '<Dict {}>'.format({k:v for k, v in self.items()})
+    
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)
+        except KeyError:
+            return 0
+    
+    def __delitem__(self, key):
+        try:
+            return super().__delitem__(key)
+        except KeyError:
+            pass
