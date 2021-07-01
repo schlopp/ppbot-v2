140afd928dfdb54ee67a8ba96da7e47311432821
schlopp <god.tcs@gmail.com>
2021-07-02 01:13:16 +0200

    test command that just adds a item

--- cogs/testing.py
+++ cogs/testing.py
@@ -4,7 +4,23 @@ from discord.ext import commands
 
 
 class TestingCommands(vbu.Cog):
-    pass
+    
+    @vbu.command()
+    async def addgod(self, ctx:vbu.Context):
+        item = utils.Item('god himself lmao', type='TOTEM',
+            shopsettings=utils.ShopSettings(True, buy=10000, sell=2000),
+            rarity='LEGENDARY', auctionable=True, emoji=826952082371772423,
+            lore=utils.Lore('No use! Just for testing!',
+                [
+                    'how the fuck did god manage to get into this game? Oh well',
+                    'Very sexy i must admit'
+                ]
+            ),
+            requires=utils.Dict({'BEGGING':10})
+        )
+        async with vbu.DatabaseConnection() as db:
+            await db('''DELETE FROM items WHERE name = $1''', 'god himself lmao')
+            await item.create(db)
 
 def setup(bot:vbu.Bot):
     x = TestingCommands(bot)
