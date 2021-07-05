3f44e00ab194a17e2602758a000a6a693052b3ad
schlopp <god.tcs@gmail.com>
2021-07-05 22:50:15 +0200

    randomnames test command

--- cogs/testing.py
+++ cogs/testing.py
@@ -55,6 +55,11 @@ class TestingCommands(vbu.Cog):
                     return await ctx.reply(f'You need **{round(50 * pp.multiplier) - pp.size}** more inches!', mention_author=False)
                 pp.multiplier = round(pp.multiplier + 0.05, 2)
                 return await ctx.reply(f'Your multiplier is now **{pp.multiplier}x**!', mention_author=False)
+    
+    @vbu.command(name='randomnames')
+    async def _random_names_command(self, ctx: vbu.Context, amount: typing.Optional[int] = 1):
+        for _ in range(amount):
+            await ctx.send(utils.random_name())
 
 
 def setup(bot: vbu.Bot):
