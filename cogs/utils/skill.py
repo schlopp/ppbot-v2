3b0adf35cbbd701d3e588393009effc5b9ee522c
schlopp <god.tcs@gmail.com>
2021-07-07 13:43:18 +0200

    styling

--- cogs/utils/skill.py
+++ cogs/utils/skill.py
@@ -12,9 +12,9 @@ def get_level_by_exp(experience: int) -> int:
         if experience >= i:
             return i
 
-
 async def update_skill(db: vbu.DatabaseConnection, user_id: int, skill_name: str, experience: int):
     await db('''
         INSERT INTO user_skill (user_id, name, experience) VALUES ($1, $2, $3)
         ON CONFLICT (user_id, name) DO UPDATE SET experience = user_skill.experience + $3
         ''', user_id, skill_name, experience)
+    
