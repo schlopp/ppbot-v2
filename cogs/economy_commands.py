fb8347ba4b20519fb60f2e52eef6f8ee0483e1d8
schlopp <rdt.lageweg@gmail.com>
2021-08-16 19:47:38 +0200

    Load items on initialization

--- cogs/economy_commands.py
+++ cogs/economy_commands.py
@@ -1,10 +1,22 @@
-import voxelbotutils as vbu
+import toml
 
+import voxelbotutils as vbu
 
 class EconomyCommands(vbu.Cog):
 
     def __init__(self, bot: vbu.Bot):
         self.bot = bot
+        
+        data = toml.load("config/items.toml")
+        try:
+            self.bot.items.clear()
+        except AttributeError:
+            pass
+        self.bot.items = {
+            "shop": {i["id"]: i for i in data["items"] if i["shop_settings"]["buyable"]},
+            "auction": {i["id"]: i for i in data["items"] if i["shop_settings"]["auctionable"]},
+            "all": {i["id"]: i for i in data["items"]},
+        }
 
 
 def setup(bot: vbu.Bot):
