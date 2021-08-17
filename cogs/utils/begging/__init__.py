a4558f6a644a6de385401dbf6d6c8fa9cd3a7d06
schlopp <god.tcs@gmail.com>
2021-08-18 00:38:06 +0200

    Update imports

--- cogs/utils/begging/__init__.py
+++ cogs/utils/begging/__init__.py
@@ -1,3 +1,3 @@
 from cogs.utils.begging.location import (
-    LootTableItem, FillInTheBlank, MiniGames, Quotes, BeggingLocation
+    LootTableItem, FillInTheBlank, MiniGames, Quotes, BeggingLocation, BeggingLocations
 )
