375d4ecf3bc9710be1443dca9b517f27e06e6005
schlopp <god.tcs@gmail.com>
2021-08-17 15:00:09 +0200

    Add new classes and methods

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,3 +1,5 @@
 from cogs.utils.item import (
     SkillRequirements, ShopSettings, Recipe, CraftingUsage, BrewingUsage, SkillUsage, Usage, Item
 )
+from cogs.utils.pp import PpWrapper, Pp
+from cogs.utils.skills import get_level_by_exp, update_skill, SkillWrapper, Skill
