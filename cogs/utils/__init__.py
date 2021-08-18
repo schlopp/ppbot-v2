a9c6a36bd1e1ce624aab11d6cfdce90fe6792e69
schlopp <god.tcs@gmail.com>
2021-08-18 19:48:03 +0200

    Add LootableItem to __init__

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,5 +1,5 @@
 from cogs.utils.item import (
-    SkillRequirements, ShopSettings, Recipe, CraftingUsage, BrewingUsage, SkillUsage, Usage, Item
+    SkillRequirements, ShopSettings, Recipe, CraftingUsage, BrewingUsage, SkillUsage, Usage, Item, LootableItem
 )
 from cogs.utils.pp import PpWrapper, Pp
 from cogs.utils.skills import get_level_by_exp, update_skill, SkillWrapper, Skill
