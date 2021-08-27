032209c0f57248a5c180480e8ccce7f2279f399a
schlopp <god.tcs@gmail.com>
2021-08-27 13:38:29 +0200

    Update get_level_by_exp

--- cogs/utils/skills.py
+++ cogs/utils/skills.py
@@ -10,17 +10,17 @@ def get_level_by_exp(exp) -> int:
     """
 
     exp_per_level = [
-        0,       50,     175,        # 1  2  3
-        375,     675,    1_175,      # 4  5  6
-        1_925,   2_925,  4_425,      # 7  8  9
-        6_425,   9_925,  14_925,     # 10 11 12
-        22_425,  32_425, 47_425,     # 13 14 15
-        67_425,  97_425, 147_425,    # 16 17 18
-        222_425, 322_425             # 19 20
+        0,       50,     175,       # 0  1  2
+        375,     675,    1_175,     # 3  4  5
+        1_925,   2_925,  4_425,     # 6  7  8
+        6_425,   9_925,  14_925,    # 9  10 11
+        22_425,  32_425, 47_425,    # 12 13 14
+        67_425,  97_425, 147_425,   # 15 16 17
+        222_425, 322_425, 472425,   # 18 19 20
     ]
     for n, i in enumerate(exp_per_level):
         if exp <= i:
-            return n + 1
+            return n
 
 async def update_skill(db: vbu.DatabaseConnection, user_id: int, skill_name: str, experience: int):
     await db("""
@@ -64,10 +64,7 @@ class Skill:
     name: str
     experience: typing.Optional[int] = 0
 
-    def __init__(
-        self, user_id: int, name: typing.Optional[str] = 'Unnamed Pp',
-        experience: typing.Optional[int] = 0
-    ):
+    def __init__(self, user_id: int, name: str, experience: typing.Optional[int] = 0):
         self.user_id = user_id
         self.name = name
         self.experience = experience
