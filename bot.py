a2af0fe6d47c2f569c1cdbed7e20f0ae2da1b926
schlöpp! <67605203+schlopp@users.noreply.github.com>
2021-09-14 00:05:11 +0200

    Add sharding support

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import discord
 import toml
 
 
-class Client(discord.Client):
+class AutoShardedClient(discord.AutoShardedClient):
     status = discord.Status.idle
 
     async def on_message(self, message):
@@ -13,6 +13,6 @@ class Client(discord.Client):
             laughing = random.choice(['kek', 'keke', 'kekw', 'lmao', 'lmfao', 'lol'])
             await message.reply(f"Sorry, but pp bot is currently offline (The dev's PC died and is being fixed {laughing})")
 
-client = Client()
+client = AutoShardedClient()
 token = toml.load(r"config.toml")["token"]
 client.run(token)
