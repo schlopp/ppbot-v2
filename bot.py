ccde9bbd0b841f3d551dc1ee607f2e1d12065040
schlöpp! <67605203+schlopp@users.noreply.github.com>
2021-09-14 20:50:46 +0200

    Send embed on command

--- bot.py
+++ bot.py
@@ -7,12 +7,25 @@ import toml
 
 class AutoShardedClient(discord.AutoShardedClient):
     def __init__(self) -> None:
-        super().__init__(status=discord.Status.idle)
+        super().__init__(
+            status=discord.Status.idle, activity=discord.Game(name="There is no spoon")
+        )
 
     async def on_message(self, message):
         if re.match(r"^(pp\s)", message.content, re.IGNORECASE):
-            laughing = random.choice(['kek', 'keke', 'kekw', 'lmao', 'lmfao', 'lol'])
-            await message.reply(f"Sorry, but pp bot is currently offline (The dev's PC died and is being fixed {laughing})")
+            embed = discord.Embed()
+            embed.title = random.choice(
+                (
+                    "Beep boop try again later.",
+                    "I'm afraid I can't do that.",
+                    "Bot broke, cry about it",
+                    "Sorry, can't.",
+                    "Nah.",
+                )
+            )
+            embed.description = "pp bot is currently __offline__, and will be online again in a few days.\n[FAQ](https://github.com/schlopp/ppbot/blob/offline/FAQ.md)"
+            await message.reply(embed=embed)
+
 
 client = AutoShardedClient()
 token = toml.load(r"config.toml")["token"]
