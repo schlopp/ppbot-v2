7381dc24b1975ef50c71607e9667684f15a32f25
schlopp <god.tcs@gmail.com>
2021-09-25 19:04:52 +0200

    Add inventory wrapper

--- cogs/utils/inventory.py
+++ cogs/utils/inventory.py
@@ -6,6 +6,30 @@ import voxelbotutils as vbu
 from . import LootableItem
 
 
+class InventoryWrapper:
+    def __init__(
+        self,
+        bot: vbu.Bot,
+        db: vbu.DatabaseConnection,
+        user_id: int,
+        update_values: typing.Optional[bool] = False,
+    ):
+        self.bot = bot
+        self.db = db
+        self.user_id = user_id
+        self.update_values = update_values
+
+    async def __aenter__(self) -> Inventory:
+        v = await self.db("SELECT * FROM user_inv WHERE user_id=$1", self.user_id)
+        items = []
+        for i in v:
+            try:
+                items.append(LootableItem.from_item(self.bot, self.bot.items["all"][i["item_id"]]))
+            except KeyError:
+                pass
+        return Inventory(self.user_id, *items)
+
+
 @dataclass
 class Inventory:
     """
@@ -30,3 +54,16 @@ class Inventory:
 
         self.user_id = user_id
         self.items = list(items)
+
+    async def update_values(self, db: vbu.DatabaseConnection):
+        """
+        Update the database with the inventory's values
+        """
+
+        await db.conn.executemany(
+            """
+            INSERT into user_inv VALUES ($1, $2, $3) ON CONFLICT DO UPDATE
+            SET amount = $3
+            """,
+            *((self.user_id, x.id, x.amount) for x in self.items)
+        )
