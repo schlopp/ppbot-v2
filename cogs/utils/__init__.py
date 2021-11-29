0241c7faecfa1a0e5ddc849a7dc37818922635f9
schlopp <god.tcs@gmail.com>
2021-11-30 00:35:29 +0100

    fix circular imports

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,17 +1,13 @@
 from .begging import *
-
-# ! Import cached_user after pp and skills
-from .cached_user import *
-from .checks import *
 from .colours import *
 from .gambling import *
-
-# ! Import inventory after item
-from .inventory import *
-from .item import *
-from .paginator import *
 from .pp import *
-from .readable import *
 from .responses import *
 from .skills import *
+from .cached_user import *
+from .item import *
+from .inventory import *
+from .readable import *
+from .paginator import *
 from .using_command import *
+from .checks import *
